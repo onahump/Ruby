37780759e46fd5e64dd5ba9288e55a286febd328
onahump <oswaldo@makingdevs.com>
2019-04-08 22:54:44 -0500

    swich case example

--- conditionals.rb
+++ conditionals.rb
@@ -29,16 +29,12 @@ puts "Ehhhhhhhhhhh pluto" if a>c
 
 # puts "Ehhhhhhhhhh...." until a<c #Print Ehhhh until A be greater than B
 
-
 valor = gets.chomp.to_i
 
 case valor
-when valor > a
- puts "Que honguirri"
-when valor == b
- puts "Somos iguales papu"
-when b > e
- puts "Esto chido"
-else
-  puts "No pues no"
+  when 1..17
+    puts "No eres mayor"
+  when 18..100
+    puts "Eres mayor"
 end
+

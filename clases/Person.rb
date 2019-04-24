82e5f71a17d67ad5aa6979bc3643181a29da2033
onahump <oswaldo@makingdevs.com>
2019-04-24 12:46:11 -0500

    Using attr_accesor to write and read

---
+++ clases/Person.rb
@@ -0,0 +1,15 @@
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def chears
+    print "Hi, my name is #{@name} and I am #{@age} years old"
+  end
+
+end
+
+

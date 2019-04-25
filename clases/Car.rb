948ab17f97f2a2e6a9b3766cce08da245d4b2b6d
onahump <oswaldo@makingdevs.com>
2019-04-24 20:04:06 -0500

    Adding class variable

--- clases/Car.rb
+++ clases/Car.rb
@@ -1,7 +1,14 @@
 class Car
 
+  @@ruedas = 4
+
+
   def self.class_method  #This is a method called just by itself
     puts "Hi, I'm a class method"
+  end
+
+  def numero_de_ruedas
+    p @@ruedas
   end
 
 end
